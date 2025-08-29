8df368fc27827c72d5e3bf0d16ac59c79a7d5661
Brenda <brend4costa.1405@gmail.com>
2025-08-29 09:15:45 -0300

    adicionando js, mundando descrição dos times, alterando html

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
 
+//  esse js controla a palavra "active" elementos, por exemplo, se vc clica em corinthians, 
+//  ele adiciona a palavra active e essa será a única descrição mostrada, 
+//  se vc clica em palmeiras, ele remove o active de corinthians e coloca no palmeiras
+
+about.addEventListener('click', function(e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function(btn) {
+      btn.classList.remove('active');
+    });
+    e.target.classList.add('active');
+
+    articles.forEach(function(article) {
+      article.classList.remove('active');
+    });
+
+    const element = document.getElementById(id);
+    element.classList.add('active');
+  }
+});
